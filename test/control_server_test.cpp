3049e10a6fdaf9f99e5b665c91ca33cac2c8c8cf
MichaelCache <faith_gentoo@163.com>
2023-06-11 08:52:14 +0800

    <feat> test cancel send

--- test/control_server_test.cpp
+++ test/control_server_test.cpp
@@ -7,12 +7,13 @@
 Q_DECLARE_METATYPE(QVector<FileInfo>)
 Q_DECLARE_METATYPE(QHostAddress)
 
+void ControlServerTest::initTestCase() { m_cs = new ControlServer(this); }
+
 void ControlServerTest::sendTest() {
   qRegisterMetaType<QVector<FileInfo>>();
   qRegisterMetaType<QHostAddress>();
 
-  ControlServer* cs = new ControlServer(this);
-  QSignalSpy spy(cs, &ControlServer::recieveFileInfo);
+  QSignalSpy spy(m_cs, &ControlServer::recieveFileInfo);
 
   QStringList files_1{"test.txt", "test2"};
   QStringList files_2{"t.d", "t2"};
@@ -35,7 +36,7 @@ void ControlServerTest::sendTest() {
     }
   }
 
-  cs->onSendFile(files_1, QHostAddress::LocalHost);
+  m_cs->onSendFile(files_1, QHostAddress::LocalHost);
   QVERIFY(spy.wait());
   QCOMPARE(spy.count(), 1);
 
@@ -46,7 +47,57 @@ void ControlServerTest::sendTest() {
     QCOMPARE(infos.at(i).m_byte, 0);
   }
 
-  cs->onSendFile(files_2, QHostAddress::LocalHost);
+  m_cs->onSendFile(files_2, QHostAddress::LocalHost);
+  QVERIFY(spy.wait());
+  QCOMPARE(spy.count(), 1);
+
+  arguments = spy.takeFirst();
+  infos = arguments.at(0).value<QVector<FileInfo>>();
+  for (int i = 0; i < infos.size(); ++i) {
+    QCOMPARE(infos.at(i).m_name, files_2.at(i));
+    QCOMPARE(infos.at(i).m_byte, 0);
+  }
+}
+
+void ControlServerTest::cancelTest() {
+  qRegisterMetaType<QVector<FileInfo>>();
+  qRegisterMetaType<QHostAddress>();
+
+  QSignalSpy spy(m_cs, &ControlServer::cancelFileInfo);
+
+  QStringList files_1{"test.txt", "test2"};
+  QStringList files_2{"t.d", "t2"};
+
+  for (auto&& i : files_1) {
+    QFile file(i);
+    if (!file.exists()) {
+      if (file.open(QIODevice::ReadWrite)) {
+        file.close();
+      }
+    }
+  }
+
+  for (auto&& i : files_2) {
+    QFile file(i);
+    if (!file.exists()) {
+      if (file.open(QIODevice::ReadWrite)) {
+        file.close();
+      }
+    }
+  }
+
+  m_cs->onCancelSend(files_1, QHostAddress::LocalHost);
+  QVERIFY(spy.wait());
+  QCOMPARE(spy.count(), 1);
+
+  auto arguments = spy.takeFirst();
+  auto infos = arguments.at(0).value<QVector<FileInfo>>();
+  for (int i = 0; i < infos.size(); ++i) {
+    QCOMPARE(infos.at(i).m_name, files_1.at(i));
+    QCOMPARE(infos.at(i).m_byte, 0);
+  }
+
+  m_cs->onCancelSend(files_2, QHostAddress::LocalHost);
   QVERIFY(spy.wait());
   QCOMPARE(spy.count(), 1);
 
--- test/control_server_test.h
+++ test/control_server_test.h
@@ -2,8 +2,15 @@
 
 #include <QtTest/QtTest>
 
+#include "server/control_server.h"
+
 class ControlServerTest : public QObject {
   Q_OBJECT
  private slots:
+  void initTestCase();
   void sendTest();
+  void cancelTest();
+
+ private:
+  ControlServer* m_cs;
 };
